c9ce62c16510f0b2e2e8bf10b6ec76dfb27f72de
xiatianming <xiatianming@yiguo.com>
2016-12-13 11:16:47 +0800

    Hook的键盘监控

---
+++ HookDemo/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace HookDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(73, 30);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(256, 25);
+            this.tbxInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入框";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 419);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HookDemo/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Windows.Forms;
+namespace HookDemo
+{
+    public partial class Form1 : Form
+    {
+        /*声明API函数*/
+        // 安装钩子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+ 
+        // 卸载钩子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        // 继续下一个钩子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, Int32 wParam, IntPtr lParam);
+
+        // 取得当前线程编号
+        [DllImport("kernel32.dll")]
+        static extern int GetCurrentThreadId();
+
+        /*声明定义*/
+        public delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);
+        static int hKeyboardHook = 0;
+        private static int hMouseHook = 0;
+        HookProc KeyboardHookProcedure;
+        HookProc MouseHookProcedure;
+
+
+        private int KeyboardHookProc(int nCode, Int32 wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                tbxInput.Text ="I'm a Keyboard";
+                return 1;
+            }
+            return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
+        }
+
+        private int MouseHookProc(int nCode, Int32 wParam, IntPtr lParam)
+        {
+            if (wParam >0)
+            {
+                tbxInput.Text = "按下键盘";
+                return 1;
+            }
+            else
+            {
+                tbxInput.Text = "键盘抬起";
+            }
+            return CallNextHookEx(hMouseHook, nCode, wParam, lParam);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 安装键盘钩子
+        /// </summary>
+        public void KeyboardHookStart()
+        {
+            if (hKeyboardHook == 0)
+            {
+                // 创建HookProc实例
+                KeyboardHookProcedure = new HookProc(KeyboardHookProc);
+
+                // 设置线程钩子
+                hKeyboardHook = SetWindowsHookEx(2, KeyboardHookProcedure, IntPtr.Zero,
+                    GetCurrentThreadId());
+
+                // 如果设置钩子失败
+                if (hKeyboardHook == 0)
+                {
+                    KeyboardHookStop();
+                    throw new Exception("SetWindowsHookEx failed.");
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 安装鼠标钩子
+        /// </summary>
+        public void MouseHookStart()
+        {
+            if (hMouseHook == 0)
+            {
+                // 创建HookProc实例
+                MouseHookProcedure = new HookProc(MouseHookProc);
+
+                // 设置线程钩子
+                hMouseHook = SetWindowsHookEx(7, MouseHookProcedure, IntPtr.Zero,
+                   GetCurrentThreadId());
+
+                // 如果设置钩子失败
+                if (hMouseHook == 0)
+                {
+                    MouseHookStop();
+                    throw new Exception("SetWindowsHookEx failed.");
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 卸载键盘钩子
+        /// </summary>
+        public void KeyboardHookStop()
+        {
+            bool retKeyboard = true;
+            bool retMouse = true;
+            if (hKeyboardHook != 0)
+            {
+                retKeyboard = UnhookWindowsHookEx(hKeyboardHook);
+                hKeyboardHook = 0;
+            }
+            if (!(retKeyboard))
+                throw new Exception("UnhookWindowsHookEx failed.");
+        }
+
+        /// <summary>
+        /// 卸载鼠标钩子
+        /// </summary>
+        public void MouseHookStop()
+        {
+            bool retMouse = true;
+            if (hMouseHook != 0)
+            {
+                retMouse = UnhookWindowsHookEx(hMouseHook);
+                hMouseHook = 0;
+            }
+            if (!(retMouse))
+                throw new Exception("UnhookWindowsHookEx failed.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KeyboardHookStart(); //安装键盘钩子
+            MouseHookStart();    //安装鼠标钩子
+        }
+    }
+}
